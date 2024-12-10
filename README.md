49ca85ec68a0eb32c701d28d2df025963952e18f
Source <lillekalle@protonmail.com>
2024-12-10 17:10:38 +0100

    Added all benchmarks and possibly fixed README.md table markdown

--- README.md
+++ README.md
@@ -5,17 +5,17 @@ Fast epoll server for Python3 (asyncio and uvloop server replacement) w/ Linux a
 Tools used: wrk with the parameters 120 connections over 6 threads.
 Hardware: server running on a 50% underclocked Intel Core Ultra 7 with 22 CPU threads.
 
-|Name|Requests per second|Connection type|
-|Asyncio|14466|close|
-|Uvloop|30608|close|
-|Fastepoll|76072|close|
-|Asyncio|109428|keep-alive|
-|Uvloop|134465|keep-alive|
-|Nginx|347217|keep-alive|
-|Fastepoll|408639|keep-alive|
+| Name | Requests per second | Connection type |
+| Asyncio | 14466 | close |
+| Uvloop | 30608 | close |
+| Fastepoll | 76072 | close |
+| Asyncio | 109428 | keep-alive |
+| Uvloop | 134465 | keep-alive |
+| Nginx | 347217 | keep-alive |
+| Fastepoll | 408639 | keep-alive |
 
 # Example code
-Note: this code won't reach benchmark performance above due to wrk being picky about required missing HTTP headers such as the date field. For the exact benchmark code used above check out the github repo.
+Note: this code won't reach benchmark performance above due to wrk being picky about required missing HTTP headers such as the date field. For the exact benchmark code used above check out bench directory.
 
 ```python
 import fastepoll
---
+++ bench/uvloop-keepalive.py
@@ -0,0 +1,27 @@
+import asyncio, signal, time, uvloop
+
+timenow = 0
+response = b""
+
+class WebServer(asyncio.Protocol):
+  def connection_made(self, transport):
+    self.transport = transport
+
+  def data_received(self, data: bytes):
+    global response, timenow
+    newtime = int(time.time())
+    if newtime != timenow:
+      timenow = newtime
+      response = b"""HTTP/1.0 200 OK\r\nServer: nginx/1.22.1\r\nDate: %b\r\nContent-Type: text/html\r\nContent-Length: 615\r\nLast-Modified: Fri, 15 Nov 2024 22:21:58 GMT\r\nConnection: keep-alive\r\nAccept-Ranges: bytes\r\n\r\n<!DOCTYPE html>\n<html>\n<head>\n<title>Welcome to nginx!</title>\n<style>\nhtml { color-scheme: light dark; }\nbody { width: 35em; margin: 0 auto;\nfont-family: Tahoma, Verdana, Arial, sans-serif; }\n</style>\n</head>\n<body>\n<h1>Welcome to nginx!</h1>\n<p>If you see this page, the nginx web server is successfully installed and\nworking. Further configuration is required.</p>\n\n<p>For online documentation and support please refer to\n<a href="http://nginx.org/">nginx.org</a>.<br/>\nCommercial support is available at\n<a href="http://nginx.com/">nginx.com</a>.</p>\n\n<p><em>Thank you for using nginx.</em></p>\n</body>\n</html>\n""" % time.strftime("%a, %d %b %Y %T %Z", time.gmtime(timenow)).encode()
+    self.transport.write(response)
+
+  def eof_received(self):
+    pass
+
+uvloop.install()
+loop = asyncio.get_event_loop()
+loop.add_signal_handler(signal.SIGINT, loop.stop)
+f = loop.create_server(WebServer, "localhost", "80")
+loop.run_until_complete(f)
+loop.run_forever()
+loop.close()
